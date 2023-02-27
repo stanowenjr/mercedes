f83c15f16f79e8069c8feb38241eac257348f6e4
Stan <stanobaseikiebor.com>
2023-02-27 15:56:09 +0000

    headers removed

--- app.java
+++ app.java
@@ -1,11 +1,5 @@
 This is great
-<<<<<<< HEAD
-=======
 developing java apps
 testing to make sure the merge was successful.
 more testing
-<<<<<<< HEAD
->>>>>>> development
-=======
 merge is successful
->>>>>>> development
