e63578de4f23ed9e32c6346e6ce7c280a4f49ab0
stanowenJ <126034793+stanowenJ@users.noreply.github.com>
2023-03-03 22:28:18 -0500

    bug removed

--- app.java
+++ app.java
@@ -1,10 +1,11 @@
-we support:
-java apps
+we support;
+java apps,
 testing to make sure the merge was successful.
 more testing
 merge is successful
 merge is successful
-.net applications 
-NodeJS applications
-python applications
+.net applications, 
+NodeJS applications,
+python applications,
 web applications
+bug removed
