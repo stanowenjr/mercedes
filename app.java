22e17ce02dc6fc814950c10bc8bf5b461690a02d
Stan <stanobaseikiebor.com>
2023-03-05 04:10:38 +0000

    modified

--- app.java
+++ app.java
@@ -4,8 +4,15 @@ testing to make sure the merge was successful.
 more testing
 merge is successful
 merge is successful
+<<<<<<< HEAD
 .net applications, 
 NodeJS applications,
 python applications,
 web applications
 bug removed
+=======
+NodeJS applications 
+great
+This app is excellent
+This app is excellent for all users
+>>>>>>> fe08ec9 (excellent app for all)
